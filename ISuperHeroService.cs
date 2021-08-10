cd66c4722924e05a7ca8957f80ccd03fb676a1de
dfnovato <87324446+dfnovato@users.noreply.github.com>
2021-08-10 16:19:10 -0300

    Add files via upload

---
+++ ISuperHeroService.cs
@@ -0,0 +1,15 @@
+﻿using SuperHeroDB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Client.Services
+{
+    public interface ISuperHeroService
+    {
+        Task<List<SuperHero>> GetSuperHeroes();
+
+        Task<SuperHero> GetSuperHero(int id);
+    }
+}
---
+++ SuperHero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Shared
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Heroname { get; set; }
+        public Comic Comic { get; set; }
+    }
+}
